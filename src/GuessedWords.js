fc533a4c0e755d1df0544101e3fb07e747a45612
nami izarra <nami.izarra@gmail.com>
2020-08-26 06:04:09 +0900

    added helper function & tests to check matching letters

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -21,7 +21,7 @@ const GuessedWords = (props) => {
 				<h3>Guessed Words</h3>
 				<table className="table table-sm">
 					<thead className="thead-light">
-						<tr><th>Guess</th><th>Matchind Letters</th></tr>
+						<tr><th>Guess</th><th>Matching Letters</th></tr>
 					</thead>
 					<tbody>
 						{ guessedWordsRows }
