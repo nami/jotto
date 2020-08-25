0f3265f1396de2f752024845dd1ca88ed9fd1e11
nami izarra <nami.izarra@gmail.com>
2020-08-26 05:45:18 +0900

    tests for guessed and non-guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,6 +9,26 @@ const GuessedWords = (props) => {
 				Try to guess the secret word!
 			</span>
 		)
+	} else {
+		const guessedWordsRows = props.guessedWords.map((word, index) => (
+			<tr data-test="guessed-word" key={index}>
+				<td>{word.guessedWord}</td>
+				<td>{word.letterMatchCount}</td>
+			</tr>
+		))
+		contents = (
+			<div data-test="guessed-words">
+				<h3>Guessed Words</h3>
+				<table>
+					<thread>
+						<tr><th>Guess</th><th>Matchind Letters</th></tr>
+					</thread>
+					<tbody>
+						{ guessedWordsRows }
+					</tbody>
+				</table>
+			</div>
+		)
 	}
 	return(
 		<div data-test="component-guessed-words">
@@ -20,10 +40,10 @@ const GuessedWords = (props) => {
 GuessedWords.propTypes = {
 	guessedWords: PropTypes.arrayOf(
 		PropTypes.shape({
-			guessedWords: PropTypes.string.isRequired,
+			guessedWord: PropTypes.string.isRequired,
 			letterMatchCount: PropTypes.number.isRequired,
 		})
-	).isRequired,
+	).isRequired
 };
 
 export default GuessedWords;
