67035be35def3bae1ab5a1d7bfbdf33870c0adf4
nami izarra <nami.izarra@gmail.com>
2020-08-26 07:37:20 +0900

    complete input component and render test for success=false

--- src/input.js
+++ src/input.js
@@ -3,15 +3,33 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
+  	const inputForm = 		
+  		(
+  			<form className="form-inline">
+				<input 
+					data-test="input-box"
+					className="mb-2 mx-sm-3"
+					type="text"
+					placeholder="Enter guess..."/>
+				<button
+					data-test="submit-button"
+					className="btn btn-primary mb-2"
+					type="submit">
+					Submit
+				</button>
+			</form>
+		)
+  	const contents = this.props.success ? null : inputForm
     return (
-      <div>
+      <div data-test="component-input">
+      	{ contents }
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-	return {}
+const mapStateToProps = ({ success }) => {
+	return { success }
 }
 
 
