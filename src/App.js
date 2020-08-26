f9df6510d252978826c68cef6c36800f5c3da6b1
nami izarra <nami.izarra@gmail.com>
2020-08-27 01:17:20 +0900

    changing test for hooks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Congrats from './Congrats';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container" data-test="component-app">
         <h1>Jotto</h1>
         <Congrats success={true} />
         <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3}]}/>
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-});
+const setup = () => {
+	return shallow(<App />)
+}
+
+test('App renders without error', () => {
+	const wrapper = setup()
+	const component = findByTestAttr(wrapper, 'component-app')
+	expect(component.length).toBe(1)
+})
