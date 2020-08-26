1c94f53d9dd7969a725d6132332ae76b737c8c6a
nami izarra <nami.izarra@gmail.com>
2020-08-26 22:25:33 +0900

    set up GuessedWords tests

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+	describe('no guesssed words', () => {
+		test('updates state correctly for unsuccessful guess', () => {
+
+		})
+		test('updates state correctly for successful guess', () => {
+
+		})
+	})
+	describe('some guesssed words', () => {
+		test('updates state correctly for unsuccessful guess', () => {
+
+		})
+		test('updates state correctly for successful guess', () => {
+
+		})
+	})
+});
