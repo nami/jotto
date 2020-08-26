f14e659261f0cdf6bfc76d59cfb7ddde696d47a6
nami izarra <nami.izarra@gmail.com>
2020-08-27 01:21:33 +0900

    setup Input component and tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container" data-test="component-app">
-        <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3}]}/>
-      </div>
-    )
-  }
+const App = (props) => {
+	return (
+	  <div className="container" data-test="component-app">
+	    <h1>Jotto</h1>
+	    <Congrats success={true} />
+	    <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3}]}/>
+	  </div>
+	)
 }
 
 export default App;
---
+++ src/Input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Input = (props) => {
+	return (
+	  <div data-test="input-component">
+	  </div>
+	)
+}
+
+export default Input;
