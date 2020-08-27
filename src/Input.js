b0c68cb5a901ad3c893ccd618ad62753c1d97e84
nami izarra <nami.izarra@gmail.com>
2020-08-27 13:05:57 +0900

    set up currentGuess state test

--- src/Input.js
+++ src/Input.js
@@ -2,8 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = (props) => {
+	const [ currentGuess, setCurrentGuess] = React.useState('')
+
 	return (
 	  <div data-test="input-component">
+	  <form className="form-inline">
+	  	<input 
+		  	data-test="input-box"
+		  	className="mb-2 mx-sm-3"
+		  	type="text"
+		  	placeholder="Enter guess..."
+		  	value={currentGuess}
+		  	onChange={(e) => setCurrentGuess(e.target.value)}
+	  	/>
+	  	<button
+		  	data-test="submit-button"
+		  	className="btn btn-primary mb-2"
+	  	>Submit</button>
+	  </form>
 	  </div>
 	)
 }
