8e77fdf111a73ca0344a859658cf1cb162a816ba
nami izarra <nami.izarra@gmail.com>
2020-08-28 02:50:50 +0900

    added getSecretWord state and tested that it mounts at start

--- src/App.js
+++ src/App.js
@@ -3,8 +3,30 @@ import './App.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import hookActions from './actions/hookActions';
+
+function reducer(state, action) {
+	switch(action.type) {
+		case 'setSecretWord':
+			return { ...state, secretWord: action.payload }
+		default:
+			throw new Error(`Invalid action type: ${action.type}`)
+	}
+}
 
 const App = (props) => {
+	const [state, dispatch] = React.useReducer(
+		reducer,
+		{ secretWord: ''}
+	)
+
+	const setSecretWord = (secretWord) => dispatch({ type: 'setSecretWord', payload: secretWord })
+
+	React.useEffect(
+		() => { hookActions.getSecretWord(setSecretWord)},
+		[]
+	)
+
 	return (
 	  <div className="container" data-test="component-app">
 	    <h1>Jotto</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 const setup = () => {
-	return shallow(<App />)
-}
+	//clear fn to make sure it doesnt carry through
+	mockGetSecretWord.mockClear()
+	// replace with mock fn
+	hookActions.getSecretWord = mockGetSecretWord
+	// use mount before useEffect is not called on 'shallow'
+	return mount(<App />)
+};
 
 test('App renders without error', () => {
 	const wrapper = setup()
 	const component = findByTestAttr(wrapper, 'component-app')
 	expect(component.length).toBe(1)
-})
+});
+
+describe('getSecretWord calls', () => {
+	test('getSecretWord gets called on App mount', () => {
+		setup()
+
+		expect(mockGetSecretWord).toHaveBeenCalled()
+	})
+});
