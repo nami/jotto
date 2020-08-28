32ddda8e69b5d927e3580e868e319a385674a4a9
nami izarra <nami.izarra@gmail.com>
2020-08-28 20:15:07 +0900

    created and setup LanguagePicker and tests

---
+++ src/LanguagePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LanguagePicker = ({ setLanguage }) => {
+	return (
+		<div className="hi">
+		</div>
+	)
+}
+
+LanguagePicker.propTypes = {
+	setLanguage: PropTypes.func.isRequired
+};
+
+
+export default LanguagePicker;
