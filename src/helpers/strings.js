a2f311487893b0d591e300b9f5a50603f8898136
nami izarra <nami.izarra@gmail.com>
2020-08-28 20:04:57 +0900

    added warnings for invalid languages with tests

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -21,6 +21,7 @@ const languageStrings = {
 
 function getStringByLanguage(languageCode, stringKey, strings=languageStrings) {
 	if (!strings[languageCode] || !strings[languageCode][stringKey]) {
+		console.warn(`Could not get string [${stringKey}] for [${languageCode}]`)
 		// fall back to english
 		return strings.en[stringKey]
 	}
